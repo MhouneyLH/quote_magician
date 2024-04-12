6e21bae669d6d23819a8f90636a270b698b0a6a1
Lucas Hünniger <lucas.huenniger02@gmail.com>
2024-04-13 01:40:17 +0200

    feat: add logging

--- src/lib/quote/api/devops_quote_api.ts
+++ src/lib/quote/api/devops_quote_api.ts
@@ -1,16 +1,22 @@
 import type { Quote } from '../quote';
 import type { QuoteAPI } from './quote_api';
 import { env } from '$env/dynamic/public';
+import { Logger } from '$lib/utils/logger';
 
 export class DevopsQuoteAPI implements QuoteAPI {
 	private API_URL: string = `http://${env.PUBLIC_QUOTE_API_ADDRESS}:4000`;
 
 	async getAllQuotes(): Promise<Quote[]> {
 		try {
+			Logger.log('Getting all quotes');
+
 			const response = await fetch(`${this.API_URL}/quotes`, {
 				method: 'GET'
 			});
 			const data = await response.json();
+
+			Logger.log('Got all quotes');
+			Logger.log(JSON.stringify(data['body']));
 
 			return JSON.parse(data['body']);
 		} catch (error) {
@@ -21,10 +27,15 @@ export class DevopsQuoteAPI implements QuoteAPI {
 
 	async getDailyQuote(): Promise<Quote> {
 		try {
+			Logger.log('Getting daily quote');
+
 			const response = await fetch(`${this.API_URL}/quotes/daily`, {
 				method: 'GET'
 			});
 			const data = await response.json();
+
+			Logger.log('Got daily quote');
+			Logger.log(JSON.stringify(data['body']));
 
 			return JSON.parse(data['body']);
 		} catch (error) {
@@ -41,6 +52,9 @@ export class DevopsQuoteAPI implements QuoteAPI {
 				likeCount: quote.likeCount
 			});
 
+			Logger.log('Creating quote');
+			Logger.log(body);
+
 			const response = await fetch(`${this.API_URL}/quote`, {
 				method: 'POST',
 				headers: {
@@ -48,6 +62,9 @@ export class DevopsQuoteAPI implements QuoteAPI {
 				},
 				body: body
 			});
+
+			Logger.log('Created quote');
+			Logger.log(JSON.stringify(response));
 
 			return response;
 		} catch (error) {
@@ -64,6 +81,9 @@ export class DevopsQuoteAPI implements QuoteAPI {
 				likeCount: quote.likeCount
 			});
 
+			Logger.log('Updating quote');
+			Logger.log(body);
+
 			const response = await fetch(`${this.API_URL}/quotes/${quote.id}`, {
 				method: 'PUT',
 				headers: {
@@ -71,6 +91,9 @@ export class DevopsQuoteAPI implements QuoteAPI {
 				},
 				body: body
 			});
+
+			Logger.log('Updated quote');
+			Logger.log(JSON.stringify(response));
 
 			return response;
 		} catch (error) {
@@ -81,9 +104,14 @@ export class DevopsQuoteAPI implements QuoteAPI {
 
 	async deleteQuote(quote: Quote): Promise<Response> {
 		try {
+			Logger.log('Deleting quote');
+			Logger.log(JSON.stringify(quote));
+
 			const response = await fetch(`${this.API_URL}/quotes/${quote.id}`, {
 				method: 'DELETE'
 			});
+
+			Logger.log('Deleted quote');
 
 			return response;
 		} catch (error) {
